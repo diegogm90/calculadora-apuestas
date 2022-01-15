d2c3696aab6355d9f0c6790f1c109d078fa9c3b1
diegogm90 <diegogm90@gmail.com>
2022-01-15 10:25:13 +0100

    onclick añadido

---
+++ js/calculadorasurebets.js
@@ -0,0 +1,7 @@
+cuota1 = document.getElementById("cuota1").value;
+cuota2 = document.getElementById("cuota2").value;
+cuota3 = document.getElementById("cuota3").value;
+
+function clickbutton() {
+    
+}
