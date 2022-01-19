fe1ac94d3d50e089a1bd2f3c22568deea3c73bd4
diegogm90 <diegogm90@gmail.com>
2022-01-19 21:25:18 +0100

    añadida recolección de datos al hacer click

--- js/calculadorasurebets.js
+++ js/calculadorasurebets.js
@@ -1,7 +1,14 @@
-cuota1 = document.getElementById("cuota1").value;
-cuota2 = document.getElementById("cuota2").value;
-cuota3 = document.getElementById("cuota3").value;
+cuota1 = 0;
+cuota2 = 0;
+cuota3 = 0;
+totalApuesta = 0;
 
+//función que recoge valores
 function clickbutton() {
-    
+    cuota1 = document.getElementById("cuota1").value;
+    cuota2 = document.getElementById("cuota2").value;
+    cuota3 = document.getElementById("cuota3").value;
+    totalApuesta = document.getElementById("total-apuesta").value;
+    console.log(totalApuesta)
 }
+
